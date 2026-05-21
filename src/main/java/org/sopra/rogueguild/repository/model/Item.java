9f28995264b325ef6606296e94dce13a569b8bdc
victor <viclongor@alu.edu.gva.es>
2026-05-21 09:54:55 +0200

    HU-03 Hecha

--- src/main/java/org/sopra/rogueguild/repository/model/Item.java
+++ src/main/java/org/sopra/rogueguild/repository/model/Item.java
@@ -4,11 +4,13 @@ public abstract class Item {
     private String name;
     private int price;
     private ItemCategory category;
+    private final int basePrice;
 
     public Item(String name, int price, ItemCategory category) {
         this.name = name;
         this.price = price;
         this.category = category;
+        basePrice = price;
     }
 
     public String getName() { return name; }
